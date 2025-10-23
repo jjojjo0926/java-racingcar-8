67bffc9d15cf950a1a22e8fa5a01f5172bffa715
jaewon926 <jjojjo0926@gmail.com>
2025-10-23 14:19:28 +0900

    feat:Car 자동차객체 생성, 전진 및 출력 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    String name;
+    int advancedTimes;
+
+    public static Car of(String name) {
+        return new Car(name, 0);
+    }
+
+    private Car(String name, int advancedTimes) {
+        this.name = name;
+        this.advancedTimes = advancedTimes;
+    }
+
+    public void advance(){
+        int advanceSpace = Randoms.pickNumberInRange(0, 9);
+        if(advanceSpace>=4){
+            advancedTimes ++;
+        }
+    }
+
+    public boolean isLeader(int lead) {
+        return this.advancedTimes >= lead;
+    }
+
+    public void printAdvancedTimes() {
+        System.out.print(this.name + " : ");
+        for(int i = 0; i < this.advancedTimes; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
