efafb2c431b3a696129aa7e37ea176c2da390cf4
jaewon926 <jjojjo0926@gmail.com>
2025-10-23 14:21:20 +0900

    feat:Race 레이스 객체 구현, 레이싱 진행 및 출력 기능 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,2 +1,64 @@
-package racingcar;public class Race {
+package racingcar;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Race {
+    Queue<Car> carsInRace;
+    int lead;
+
+    public Race(){
+        this.carsInRace = new LinkedList<>();
+        this.lead = 0;
+    }
+
+    public void participateInRace(String carName){
+        carsInRace.offer(Car.of(carName));
+    }
+
+    public void run(){
+        Iterator<Car> iter = carsInRace.iterator();
+        while(iter.hasNext()){
+            Car car = iter.next();
+            car.advance();
+            if(car.isLeader(lead)){
+                lead = car.advancedTimes;
+            }
+        }
+    }
+
+    public void printCarLocation(){
+        Iterator<Car> iter = carsInRace.iterator();
+        while(iter.hasNext()){
+            Car car = iter.next();
+            car.printAdvancedTimes();
+        }
+
+        System.out.println();
+    }
+
+    public void printResult() {
+        System.out.print("최종 우승자 : ");
+        Iterator<Car> iter = carsInRace.iterator();
+        Queue<Car> winners = new LinkedList<>();
+        while(iter.hasNext()){
+            Car car = iter.next();
+            if(car.isLeader(lead)){
+                winners.add(car);
+            }
+        }
+
+        boolean isFirst = true;
+        while(!winners.isEmpty()){
+            Car car = winners.poll();
+
+            if(isFirst){
+                isFirst = false;
+                System.out.print(car.name);
+            }else{
+                System.out.print(", " + car.name);
+            }
+        }
+    }
 }
