290e84995cc42b41cf583f87847bf9051d49c404
jaewon926 <jjojjo0926@gmail.com>
2025-10-25 21:58:14 +0900

    feat:ConsoleIOHandler, StringConverter IOHandler 및 사용자 입력 값 형식 변환, 입력값 형식 확인 기능 구현

--- src/main/java/racingcar/io/ConsoleIOHandler.java
+++ src/main/java/racingcar/io/ConsoleIOHandler.java
@@ -1,5 +1,61 @@
 package racingcar.io;
 
-public class ConsoleInputHandler {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Car;
+import racingcar.Race;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ConsoleIOHandler {
+    private final StringConverter stringConverter = new StringConverter();
+
+    public Queue<String> getCarsNameFromUser() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String nameOfCars = Console.readLine();
+
+        return stringConverter.getCarsNameFrom(nameOfCars);
+    }
+
+    public int getTrialCountFromUser() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        return stringConverter.getTrialCountFrom(Console.readLine());
+    }
+
+    public void printRaceSituation(Race race) {
+        Iterator<Car> iter = race.carsIterator();
+        while (iter.hasNext()) {
+            Car car = iter.next();
+            car.printAdvancedTimes();
+        }
+
+        System.out.println();
+    }
+
+    public void printRaceWinner(Race race) {
+        System.out.print("최종 우승자 : ");
+        Iterator<Car> iter = race.carsIterator();
+        Queue<String> winners = new LinkedList<>();
+
+        while (iter.hasNext()) {
+            Car car = iter.next();
+            if (race.isLeader(car)) {
+                winners.add(car.getName());
+            }
+        }
+
+        boolean isFirst = true;
+        while (!winners.isEmpty()) {
+            String winningCarName = winners.poll();
+
+            if (isFirst) {
+                isFirst = false;
+                System.out.print(winningCarName);
+            } else {
+                System.out.print(", " + winningCarName);
+            }
+        }
+    }
 }
--- src/main/java/racingcar/io/StringConverter.java
+++ src/main/java/racingcar/io/StringConverter.java
@@ -1,4 +1,34 @@
 package racingcar.io;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class StringConverter {
+    public Queue<String> getCarsNameFrom(String carsNameList) {
+        Queue<String> nameOfCars = new LinkedList<>();
+
+        carsNameList = carsNameList.replace(" ", "");
+        StringTokenizer st = new StringTokenizer(carsNameList, ",");
+
+        while (st.hasMoreTokens()) {
+            String carName = st.nextToken();
+
+            nameOfCars.offer(carName);
+        }
+
+        return nameOfCars;
+    }
+
+    public int getTrialCountFrom(String trialCount) {
+        int count = 0;
+        for (int i = 0; i < trialCount.length(); i++) {
+            if (trialCount.charAt(i) >= '0' && trialCount.charAt(i) <= '9') {
+                count = count * 10 + (trialCount.charAt(i) - '0');
+                continue;
+            }
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+        return count;
+    }
 }
